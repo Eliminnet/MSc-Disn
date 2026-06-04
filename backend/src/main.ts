4e6f75d2dfda2579ccf312e20ecb998e345bbd56
Nathanial Patrick <nathan@psfamily.co.uk>
2026-06-04 02:11:23 +0100

    Enable CORS and default response handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { AppModule } from "@/app.module";
+import { ResponseInterceptor } from "@/shared/interceptors/response.interceptor";
+import { GlobalExceptionFilter } from "@/shared/filters/exception.filter";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalInterceptors(new ResponseInterceptor());
+	app.useGlobalFilters(new GlobalExceptionFilter());
+
+	app.enableCors({
+		origin: ["http://localhost:5173", "http://192.168.50.164"],
+		credentials: true,
+	});
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
