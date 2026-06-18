b5e94c31635026e17e08871de0112167a440549d
Eliminnet <nate@elimin.net>
2026-06-18 16:59:56 +0100

    Create navbar

--- frontend/src/ui/core/Navbar.tsx
+++ frontend/src/ui/core/Navbar.tsx
@@ -1,5 +1,39 @@
-const Navbar = () => {
-	return <div>Navbar</div>;
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+interface NavbarProps {
+	title: string;
+}
+
+const Navbar = ({ title }: NavbarProps) => {
+	return (
+		<>
+			<nav className="nav">
+				<Link
+					to="/"
+					className="nav-button nav-button__main"
+				>
+					<img
+						src="/home.svg"
+						className="nav__logo"
+					/>
+					<span>{title}</span>
+				</Link>
+				<Link
+					to=""
+					className="nav-button"
+				>
+					<span>test</span>
+				</Link>
+				<Link
+					to=""
+					className="nav-button"
+				>
+					<span>test</span>
+				</Link>
+			</nav>
+		</>
+	);
 };
 
 export default Navbar;
