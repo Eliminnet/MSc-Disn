62796a74ee0d44a8a4e5b4d7b42e39efe620b433
Eliminnet <nate@elimin.net>
2026-06-18 17:03:07 +0100

    Create default app layout with navbar

--- frontend/src/app/layout/AppLayout.tsx
+++ frontend/src/app/layout/AppLayout.tsx
@@ -1,13 +1,14 @@
+import { Page } from "@/ui";
 import Navbar from "@/ui/core/Navbar";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
 	return (
-		<>
-			<Navbar />
-			<main>
+		<Page>
+			<Navbar title="MSc Project" />
+			<main className="page-content">
 				<Outlet />
 			</main>
-		</>
+		</Page>
 	);
 }
