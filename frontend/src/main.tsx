1c74254f0534dd979b08f2bcd383242587200a6e
Eliminnet <nate@elimin.net>
2026-06-18 16:58:13 +0100

    Import index.css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { router } from "./app/routes/router";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
