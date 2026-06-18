f6e805608469b5cc2fd2a130fafec85e356cd1af
Eliminnet <nate@elimin.net>
2026-06-18 17:02:19 +0100

    Create home layout

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { handleCreate } from "../../ws/handleCreate";
 import { useJoinRoom } from "./hooks/useJoinRoom";
 
+import "./Home.css";
+
 const Home = () => {
 	const { roomId, error, setRoomId } = useJoinRoom();
 
@@ -9,28 +11,33 @@ const Home = () => {
 
 	return (
 		<>
-			<div>Home</div>
-			{roomId && <h2>{roomId}</h2>}
-			<button
-				onClick={async () => {
-					const res = await handleCreate();
-					console.log("done");
-					console.log(res);
-					if (res.ok) {
-						navigate(`/room/${res.roomId}`);
-					}
-				}}
-			>
-				Create
-			</button>
-			<input
-				type="text"
-				value={roomId}
-				onChange={(e) => setRoomId(e.target.value.toUpperCase())}
-				placeholder="Enter room code"
-				maxLength={4}
-			/>
-			{error && <div>{error}</div>}
+			<div className="home__content">
+				<div className="room-search">
+					<input
+						type="text"
+						value={roomId}
+						onChange={(e) => setRoomId(e.target.value.toUpperCase())}
+						placeholder="Enter room code"
+						maxLength={4}
+						className="room-search__input"
+					/>
+					<span className="room-search__error">{error}</span>
+				</div>
+				<span style={{ margin: "0.8rem 0" }}>or</span>
+				<button
+					className="room-create"
+					onClick={async () => {
+						const res = await handleCreate();
+						console.log("done");
+						console.log(res);
+						if (res.ok) {
+							navigate(`/room/${res.roomId}`, { state: { host: true } });
+						}
+					}}
+				>
+					Create your own!
+				</button>
+			</div>
 		</>
 	);
 };
