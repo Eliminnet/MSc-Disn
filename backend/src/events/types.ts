6a585ab34008eb93d94b47c8288a5fd547014cef
Eliminnet <nate@elimin.net>
2026-06-18 16:56:23 +0100

    Create WSResponse types

--- backend/src/events/types.ts
+++ backend/src/events/types.ts
@@ -1,3 +1,14 @@
+type WSSuccessResponse<T> = {
+	ok: true;
+} & T;
+
+type WSErrorResponse = {
+	ok: false;
+	error?: string;
+};
+
+export type WSResponse<T = void> = WSSuccessResponse<T> | WSErrorResponse;
+
 export type RoomState = {
 	hostId: string;
 	slide: number;
