f490ea049660b9ddb5a2c2c95a1a4a9ee3fe62e8
Nathanial Patrick <nathan@psfamily.co.uk>
2026-06-18 09:28:26 +0100

    Create Home buttons

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { handleCreate } from "../../ws/handleCreate";
+import { useJoinRoom } from "./hooks/useJoinRoom";
+
 const Home = () => {
+	const { roomId, error, setRoomId } = useJoinRoom();
+
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<div>Home</div>
-			<button>Create</button>
-			<button>Join</button>
+			{roomId && <h2>{roomId}</h2>}
+			<button
+				onClick={async () => {
+					const res = await handleCreate();
+					console.log("done");
+					console.log(res);
+					if (res.ok) {
+						navigate(`/room/${res.roomId}`);
+					}
+				}}
+			>
+				Create
+			</button>
+			<input
+				type="text"
+				value={roomId}
+				onChange={(e) => setRoomId(e.target.value.toUpperCase())}
+				placeholder="Enter room code"
+				maxLength={4}
+			/>
+			{error && <div>{error}</div>}
 		</>
 	);
 };
